db2b13d418d6288e9d5985b6af2b9728a30bf646
Aman Kumar Shah <175734570+Aman-prog-dev@users.noreply.github.com>
2025-03-30 14:04:30 +0530

    Add files via upload

---
+++ Quiz Application with Timer/js/questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What types of tag?",
+    answer: "Two types",
+    options: [
+      "None",
+      "One types",
+      "Two types",
+      "Three types"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What are the bold tag?",
+    answer: "b tag",
+    options: [
+      "b tag",
+      "bd tag",
+      "bold tag",
+      "bld tag"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What are Two Types of tag?",
+    answer: "Empty & Container tag",
+    options: [
+      "Element & Container Tag",
+      "Empty & Container tag",
+      "attributes & Element",
+      "container & element"
+    ]
+  },
+    {
+    numb: 6,
+    question: "Bold the data but not use <b> attribute?",
+    answer: "strong tag",
+    options: [
+      "p tag",
+      "bold tag",
+      "br tag",
+      "strong tag"
+    ]
+  },
+  {
+    numb: 7,
+    question: "what is attribute the line break?",
+    answer: "br tag",
+    options: [
+      "br tag",
+      "break tag",
+      "brek tag",
+      "brk tag"
+    ]
+  },
+  {
+    numb: 8,
+    question: "what is the element of marquee tag?",
+    answer: "All of above",
+    options: [
+      "hspace",
+      "vspace",
+      "loop",
+      "All of above"
+    ]
+  },
+  {
+    numb: 9,
+    question: "what create a table row element?",
+    answer: "tr tag",
+    options: [
+      "table tag",
+      "td tag",
+      "Th tag",
+      "tr tag"
+    ]
+  },
+  {
+    numb: 10,
+    question: "How to use In html?",
+    answer: "Web page creation",
+    options: [
+      "Web page creation",
+      "web site publish",
+      "Data handling",
+      "Web site uploding"
+    ]
+  },
+];
